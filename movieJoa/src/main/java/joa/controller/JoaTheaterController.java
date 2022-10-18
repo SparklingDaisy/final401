b9a3883387ee1b2f83806f5aeb677ce44596b7bb
DESKTOP-A4VVJHE\윤지혜 <dbsquddlfz@naver.com>
2022-10-18 14:21:48 +0900

    2022-10-18 byung il

--- movieJoa/src/main/java/joa/controller/JoaTheaterController.java
+++ movieJoa/src/main/java/joa/controller/JoaTheaterController.java
@@ -1,5 +1,14 @@
 package joa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class JoaTheaterController {
 
+	
+	@RequestMapping("/joaTheater.do")
+	public String base() {
+		return "joaTheater/joaTheater_theater";
+	}
 }
